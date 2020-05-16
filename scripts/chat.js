9f27fb8e362f344b26d5e84ea148c99f46c5253e
Josue Bravo <jbravo.post@gmail.com>
2020-05-16 12:25:44 -0700

    outlined chat app features

---
+++ scripts/chat.js
@@ -0,0 +1,4 @@
+// adding new chat documents to the 'chats' collection
+// setting up real-time listener to get new chats whenever they are added to the database
+// updating the username when user submits from said form 
+// updating the room when user clicks on on of the rooms near the top
