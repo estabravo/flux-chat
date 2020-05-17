46d7abf2ed8acf582d167efa713f05313aa904a3
Josue Bravo <jbravo.post@gmail.com>
2020-05-16 17:34:07 -0700

    created constructor for each chatroom instance

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,3 +2,13 @@
 // setting up real-time listener to get new chats whenever they are added to the database
 // updating the username when user submits from said form 
 // updating the room when user clicks on on of the rooms near the top
+
+// this is going to setup the initial properties on the chatroom instance
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+    }
+}
+
