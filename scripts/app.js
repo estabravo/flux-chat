ba1c1d99e06954abd1715b148e0c0354b00f3749
Josue Bravo <jbravo.post@gmail.com>
2020-06-01 19:24:48 -0700

    connected user input to 'addChat' on submit event

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,10 @@ const newChatForm = document.querySelector('.new-chat');
 // add a new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
-    
+    const message = newChatForm.message.value.trim()
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err)); 
 });
 
 // class instances
