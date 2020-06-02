675c89853eceb74bf8ee8876567ea7ed2b59db6d
Josue Bravo <jbravo.post@gmail.com>
2020-06-01 19:40:50 -0700

    set up event listener for submitting new usernames

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 // dom queries
 const chatList  = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -10,6 +11,12 @@ newChatForm.addEventListener('submit', e => {
         .then(() => newChatForm.reset())
         .catch(err => console.log(err)); 
 });
+
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim()
+})
 
 // class instances
 const chatUI = new ChatUI(chatList); 
