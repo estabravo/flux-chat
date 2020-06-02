033a514fa6238b8ef452122cd8656ef5d86d92e2
Josue Bravo <jbravo.post@gmail.com>
2020-06-01 19:07:59 -0700

    set up event listener for submitting new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 // dom queries
 const chatList  = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+});
 
 // class instances
 const chatUI = new ChatUI(chatList); 
